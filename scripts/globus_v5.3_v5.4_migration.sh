e4c74b4a9092f615fa06e519ff3e106dfd7503f5
Chun Ly <astro.chun@gmail.com>
2020-12-15 16:00:58 -0700

    Tweak script
     - specify IP
     - Tweak loop

--- scripts/globus_v5.3_v5.4_migration.sh
+++ scripts/globus_v5.3_v5.4_migration.sh
@@ -12,11 +12,11 @@ sudo apt install globus-connect-server54
 cp /etc/globus-connect-server.conf /etc/globus-connect-server.conf.v5.3
 for c_file in /etc/globus/*.conf
 do
-  echo $c_file $c_file.v5.3
+  cp $c_file $c_file.v5.3
 done
 
 # Migrate the endpoint
-sudo globus-connect-server endpoint migrate53 --owner chunly@arizona.edu
+sudo globus-connect-server endpoint migrate53 --owner chunly@arizona.edu --ip-address <IP_ADDRESS>
 
 # Note will need to update mapped collections
 # See: https://docs.globus.org/globus-connect-server/v5.4/migration-guide/#update_collections
